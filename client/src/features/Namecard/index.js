0b9201d531e29635871b526422446fae2a82ff67
Ken Lee <changbok89@gmail.com>
2017-11-19 02:24:36 -0500

    adding some basic css etc

--- client/src/features/Namecard/index.js
+++ client/src/features/Namecard/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Box from './components/Box';
 
 class NamecardContainer extends Component {
   render() {
-    console.log(this.props);
-    return <div>some garbo</div>;
+    const { name } = this.props.match.params;
+    return <Box content={`${name} loves donkeys`} />;
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//eslint-disable-next-line
+import styles from './styles.css';
 
 const rootEl = document.getElementById('application');
 const render = Component => {
